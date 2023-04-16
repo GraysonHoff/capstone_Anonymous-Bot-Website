7925cd2a69a6b27d4f769ef849d30d7636903ad0
Grayson Hoff <the.graysonhoff@gmail.com>
2023-04-16 18:14:24 -0500

    local working

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 require("dotenv").config();   // Read environment variables from .env file
+const { response } = require("express");
+const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 5163;
+
+express()
+  .use(express.static(path.join(__dirname, "public")))
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", async(req, res) => {
+
+    res.send("Hello Anonymous Bot!");
+  })
+  
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
